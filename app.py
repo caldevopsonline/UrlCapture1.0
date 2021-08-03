cc441fe580d9efc33e82131d957f2efdacafb70c
root <calebvoicemail@gmail.com>
2021-08-03 19:58:14 +0100

    application run instances added

--- app.py
+++ app.py
@@ -2,9 +2,18 @@ from flask import Flask, render_template, request
 from bs4 import BeautifulSoup
 import requests
 
+# Flask initialization
 app = Flask(__name__)
 
 
+# Setting up the route of the application
 @app.route('/')
+# Function to connect script to HTML interface app.html
 def main():
-    return 'app.html'
+    return render_template('app.html')
+
+
+# Begin application running process
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
